ae7cf5a6f845bc949e47f04c85a78c9d5dd83714
Ezzelyno Haggon <ehaggon@e3r1p12.21-school.ru>
2019-03-21 13:05:37 +0300

    15/15 net, 20/24 system, 0/3 script

---
+++ init/system/17.sh
@@ -0,0 +1,4 @@
+# Find the command that allows you to connect via ssh on the VM.(In parallel with the graphic session)
+ssh remote_username@remote_host
+# Find the command that allows you to connect via ssh on the VM.(In parallel with the graphic session)
+ssh remote_username@remote_host
